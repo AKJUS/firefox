fbae83314c244b9f86dd88c76234b1d7b3ae13d2
Chris H-C <chutten@mozilla.com>
2026-09-11 14:13:48 +0000

    Bug 1977639 - Report slow SQL over Glean r=mak
    sqlite3_normalized_sql is only available for prepared statements, so
    this specifically does not report on simple sql executions.
    
    This does not remove the existing Legacy Telemetry reporting (yet).
    
    Differential Revision: https://phabricator.services.mozilla.com/D321975

--- storage/mozStorageService.cpp
+++ storage/mozStorageService.cpp
@@ -24,6 +24,7 @@
 #include "mozilla/LateWriteChecks.h"
 #include "mozIStorageCompletionCallback.h"
 #include "mozIStoragePendingStatement.h"
+#include "mozilla/glean/GleanPings.h"
 #include "mozilla/StaticPrefs_security.h"
 #include "mozilla/StaticPrefs_storage.h"
 #include "mozilla/intl/Collator.h"
@@ -312,6 +313,11 @@ static const char* sObserverTopics[] = {"memory-pressure",
 
 nsresult Service::initialize() {
   MOZ_ASSERT(NS_IsMainThread(), "Must be initialized on the main thread");
+
+#ifdef NIGHTLY_BUILD
+  // Submit last session's slow SQL, if any.
+  glean_pings::SlowSql.Submit("startup"_ns);
+#endif  // NIGHTLY_BUILD
 
   int rc = AutoSQLiteLifetime::getInitResult();
   if (rc != SQLITE_OK) {
