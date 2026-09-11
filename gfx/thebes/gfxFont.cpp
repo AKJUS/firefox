ea6b888b0b8aa968ade17700c79e06170f77ac58
Adham-A <57870101+Adham-A@users.noreply.github.com>
2026-09-11 17:05:30 +0000

    Bug 1985986 - Fix RTL glyph ink overflow measurement for contained glyphs. r=layout-reviewers,jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D324373

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -3002,8 +3002,9 @@ bool gfxFont::MeasureGlyphs(const gfxTextRun* aTextRun, uint32_t aStart,
             aExtents->GetContainedGlyphWidthAppUnitsLocked(glyphIndex);
         if (extentsWidth != gfxGlyphExtents::INVALID_WIDTH &&
             aBoundingBoxType == LOOSE_INK_EXTENTS) {
-          UnionRange(x, aAdvanceMin, aAdvanceMax);
-          UnionRange(x + extentsWidth, aAdvanceMin, aAdvanceMax);
+          double glyphStart = aIsRTL ? x + advance - extentsWidth : x;
+          UnionRange(glyphStart, aAdvanceMin, aAdvanceMax);
+          UnionRange(glyphStart + extentsWidth, aAdvanceMin, aAdvanceMax);
         } else {
           gfxRect glyphRect;
           if (!aExtents->GetTightGlyphExtentsAppUnitsLocked(
