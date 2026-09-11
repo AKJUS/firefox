033035effccf8fab4e0a2e5a705fc1e6e3985d9b
calu <calu@mozilla.com>
2026-09-11 20:41:44 +0000

    Bug 2068164 - Stand up the Tab Groups strip Redux boilerplate and telemetry middleware r=android-reviewers,007
    Differential Revision: https://phabricator.services.mozilla.com/D325462

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabgroups/strip/redux/TabGroupsStripState.kt
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tabgroups.strip.redux
+
+import androidx.compose.runtime.Immutable
+import mozilla.components.lib.state.State
+
+/** The state held by the [TabGroupsStripStore]. */
+@Immutable class TabGroupsStripState : State
