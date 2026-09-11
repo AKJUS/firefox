1b4d6cc64d0874158adac6a9848268fd8c1aee16
Sandor Molnar <smolnar@mozilla.com>
2026-09-12 00:12:24 +0300

    Revert "Bug 2070984 - Use inputmode=search for the search bars. r=mak" for causing mochitest leak failures
    This reverts commit e0f4201b41159dfefc0eaf7edcb58b0663e341b3.

--- browser/components/urlbar/content/SmartbarInput.mjs
+++ browser/components/urlbar/content/SmartbarInput.mjs
@@ -169,7 +169,7 @@ ${
                       aria-controls="urlbar-results"
                       role="combobox"
                       aria-autocomplete="both"
-                      inputmode="search"
+                      inputmode="mozAwesomebar"
                       preserveundohistory=""
                       data-l10n-id="smartbar-placeholder"/>
         </html:div>
